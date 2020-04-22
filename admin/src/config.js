7d1bc743c7e829129b0fd5330ac38ade19874376
hung <hung.dang@c2c-techhub.io>
2020-04-22 15:09:53 +0700

    new att

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,3 +1,5 @@
-export const LOGIN_LOGO = null;
+import Logo from './assets/images/logo-strapi.png'
+
+export const LOGIN_LOGO = Logo;
 export const SHOW_TUTORIALS = false;
 export const SETTINGS_BASE_URL = '/settings';
